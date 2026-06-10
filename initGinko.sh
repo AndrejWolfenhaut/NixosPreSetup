0684b8435e119867e7f509185ffc09d0a1255cf7
AndrejWolfenhaut <82394127+AndrejWolfenhaut@users.noreply.github.com>
2026-06-10 18:18:50 +0200

    - Create simple initialization script

---
+++ initGinko.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+
+# Wipe existing partitions (we assume that there are three...)
+# Create GPT partition table
+# Create boot, root and swap partitions
+sudo parted /dev/nvme0n1 --script \
+  rm 1 \
+  rm 2 \
+  rm 3 \
+  mklabel gpt \
+  mkpart boot fat32 1MB 512MB \
+  mkpart root ext4 512MB -8GB \
+  mkpart swap linux-swap -8GB 100% \
+  set 1 esp on
+
+# Format the boot, root and swap partitions
+sudo mkfs.fat -F 32 -n boot /dev/nvme0n1p1
+sudo mkfs.ext4 -L root /dev/nvme0n1p2
+sudo mkswap -L swap /dev/nvme0n1p3
+
+# Mount newly created partitions
+sudo mount /dev/disk/by-label/root /mnt
+sudo mkdir -p /mnt/boot
+sudo mount -o umask=077 /dev/disk/by-label/boot /mnt/boot
+sudo swapon /dev/disk/by-label/swap
+
+# Generate config and install (keep the default config for now...)
+sudo nixos-generate-config --root /mnt
+sudo nixos-install
+
+# Reboot
+read -s -p "Basic NixOS installation complete. Press ENTER to reboot..."
