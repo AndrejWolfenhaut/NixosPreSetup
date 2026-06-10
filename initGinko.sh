6cab7c91ab1a303c850b895f6dfd5d5685d71729
AndrejWolfenhaut <82394127+AndrejWolfenhaut@users.noreply.github.com>
2026-06-10 18:51:52 +0200

    - Print status updates

--- initGinko.sh
+++ initGinko.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
-# Wipe existing partitions
-# Create GPT partition table
-# Create boot, root and swap partitions
+echo
+echo "Recreating GPT partition table,"
+echo "creating boot, root and swap partitions..."
+echo
 sudo parted /dev/nvme0n1 --script -- \
   mklabel gpt \
   mkpart boot fat32 1MB 512MB \
@@ -10,20 +11,45 @@ sudo parted /dev/nvme0n1 --script -- \
   mkpart swap linux-swap -8GB 100% \
   set 1 esp on
 
-# Format the boot, root and swap partitions
+echo
+echo "Formatting boot partition..."
+echo
 sudo mkfs.fat -F 32 -n boot /dev/nvme0n1p1
+
+echo
+echo "Formatting root partition..."
+echo
 sudo mkfs.ext4 -L root /dev/nvme0n1p2
+
+echo
+echo "Formatting swap partition..."
+echo
 sudo mkswap -L swap /dev/nvme0n1p3
 
-# Mount newly created partitions
+echo
+echo "Mounting root partition at /mnt..."
+echo
 sudo mount /dev/disk/by-label/root /mnt
+
+echo
+echo "Mounting boot partition at /mnt/boot..."
+echo
 sudo mkdir -p /mnt/boot
 sudo mount -o umask=077 /dev/disk/by-label/boot /mnt/boot
+
+echo
+echo "Mounting swap partition..."
+echo
 sudo swapon /dev/disk/by-label/swap
 
-# Generate config and install (keep the default config for now...)
+echo
+echo "Generating NixOS config..."
+echo
 sudo nixos-generate-config --root /mnt
+
+echo
+echo "Installing NixOS..."
+echo
 sudo nixos-install
 
-# Reboot
 read -s -p "Basic NixOS installation complete. Press ENTER to reboot..."
